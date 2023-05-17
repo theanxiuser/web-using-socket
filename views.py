4f5f184836c2d707fa2c33391bbb4a54f275fce2
Bishal Poudel <bishalpoudel607@gmail.com>
2023-05-17 20:05:08 +0545

    views for login and success added

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 import os
 import mimetypes
+import urllib.parse
+import threading
 
 
-def prepare_response(file, content_type):
+def prepare_response(file, content_type=("text/html",)):
     """Accept file path and content type adn return appropriate response"""
     if os.path.exists(file):
         with open(file, "rb") as f:
@@ -17,6 +19,55 @@ def prepare_response(file, content_type):
     resp += "\r\n"
     resp = resp.encode() + content
     return resp
+
+
+def success(client_sock):
+    template = "templates/success.html"
+    content_type = mimetypes.guess_type(template)
+    resp = prepare_response(template, content_type)
+    client_sock.sendall(resp)
+
+
+def redirect_client(client_sock, url):
+    # Construct the redirect response
+    redirect_response = f"HTTP/1.1 302 Found\r\nLocation: {url}\r\n\r\n"
+    client_sock.sendall(redirect_response.encode())
+    # Close the client socket after redirecting
+    client_sock.close()
+
+
+def login(client_sock, req):
+    # Extract method
+    request_line = req.split("\n")[0]
+    method = request_line.split(" ")[0]
+    print("method ", method)
+
+    if method == "POST":
+        # extract body
+        split_req = req.split("\r\n\r\n", 1)
+        body = split_req[1] if len(split_req) > 1 else ""
+        parsed_body = urllib.parse.parse_qs(body)
+
+        username = parsed_body.get('username', [''])[0]
+        password = parsed_body.get('password', [''])[0]
+
+        if username == "bishal" and password == "bishal":
+            # Redirect to home page in a separate thread
+            redirect_thread = threading.Thread(target=redirect_client, args=(client_sock, "/success"))
+            redirect_thread.start()
+
+        else:
+            # Send login form again
+            template = "templates/login.html"
+            content_type = mimetypes.guess_type(template)
+            resp = prepare_response(template, content_type)
+            client_sock.sendall(resp)
+
+    else:
+        template = "templates/login.html"
+        content_type = mimetypes.guess_type(template)
+        resp = prepare_response(template, content_type)
+        client_sock.sendall(resp)
 
 
 def index(client_sock):
